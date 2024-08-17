5b38c030ac46755da6f927a2bd511ed98fbf4924
Yusuke <14057556+yuike@users.noreply.github.com>
2024-08-17 19:58:34 +0900

    decoratorを使うと柔軟性がなくなるので削除

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,16 +1,7 @@
 import type { Preview } from "@storybook/react"
 import "../src/app/globals.css"
-import { Provider } from "jotai"
-import React from "react"
 
 const preview: Preview = {
-  decorators: [
-    (Story) => (
-      <Provider>
-        <Story />
-      </Provider>
-    ),
-  ],
   parameters: {
     controls: {
       matchers: {
