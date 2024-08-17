b555dc840988a1630cbcf3b283954631b31b7b4e
Yusuke <14057556+yuike@users.noreply.github.com>
2024-08-17 19:59:19 +0900

    リファクタリングで使わなくなったので削除

--- src/types/TaskAtom.ts
+++
@@ -1,8 +0,0 @@
-import type { WritableAtom } from "jotai"
-import type { Task } from "./Task"
-
-export type TaskAtom = WritableAtom<
-  Task | undefined,
-  [(prev: Task | undefined) => Task | undefined],
-  void
->
