6a662c9f98fcfe734213b4507bd09406de37b32a
Yusuke <14057556+yuike@users.noreply.github.com>
2024-08-20 22:35:00 +0900

    next/fontの設定をstorybookに追加

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,20 @@
 import type { Preview } from "@storybook/react"
+import { Inter } from "next/font/google"
 import "../src/app/globals.css"
+import React from "react"
+
+const inter = Inter({ weight: ["600", "700"], subsets: ["latin"] })
 
 const preview: Preview = {
+  decorators: [
+    (Story) => {
+      return (
+        <div className={inter.className}>
+          <Story />
+        </div>
+      )
+    },
+  ],
   parameters: {
     controls: {
       matchers: {
