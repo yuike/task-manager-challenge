a3ab9b0bb615c46f40f5f31e933f6b99e3d9c23b
Yusuke <14057556+yuike@users.noreply.github.com>
2024-08-20 22:35:40 +0900

    足りないテストを追加

--- src/components/TaskCard/Radio/Radio.stories.tsx
+++ src/components/TaskCard/Radio/Radio.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react"
+import { userEvent, within } from "@storybook/test"
+import { Provider } from "jotai"
 import { Radio } from "./Radio"
 
 const meta = {
@@ -10,9 +12,23 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
+  render: (args) => (
+    <Provider>
+      <Radio {...args} />
+    </Provider>
+  ),
   args: {
     label: "3 days later",
     name: "due",
     value: "3",
+  },
+}
+
+export const Selected: Story = {
+  ...Default,
+  play: async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+    const canvas = within(canvasElement)
+    const radio = canvas.getByRole("radio", { name: "3 days later" })
+    await userEvent.click(radio)
   },
 }
