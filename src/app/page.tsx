645b8537d67611c260fcdffe55563876d35d8207
Yusuke <14057556+yuike@users.noreply.github.com>
2024-08-25 21:39:21 +0900

    AddButtonをpageからTaskListへ移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,6 @@ export default function Home() {
   return (
     <section className="mx-4 my-12">
       <h2 className="text-2xl">My Tasks</h2>
-      <div className="absolute top-10 right-4">
-        <AddButton />
-      </div>
       <div className="mt-12">
         <Suspense fallback={<div>Loading...</div>}>
           <TaskList />
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -3,6 +3,7 @@
 import { ErrorMessage } from "@/components/ErrorMessage"
 import { TaskCard, TaskCardGroup } from "@/components/TaskCard"
 import type { FC } from "react"
+import { AddButton } from "../AddButton"
 import { useTaskList } from "./TaskList.hooks"
 
 export const TaskList: FC = () => {
@@ -20,6 +21,9 @@ export const TaskList: FC = () => {
 
   return (
     <>
+      <div className="absolute top-10 right-4">
+        <AddButton />
+      </div>
       {errorMessage && (
         <div className="mb-8">
           <ErrorMessage />
